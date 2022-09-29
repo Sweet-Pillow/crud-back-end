9f83936dfbe6e78efc5d3b7c09583b27a560754b
Daniel Alves Farias <daniel_h.m@outlook.com>
2022-09-29 11:45:36 -0300

    Apagado codigo sobresalente

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -14,22 +14,6 @@ namespace crud_back_end.Controllers{
         private readonly IContatoRepositorie _contatoRepositorie;
         public ContatoController(IContatoRepositorie contatoRepositorie) => _contatoRepositorie = contatoRepositorie;
 
-        
-        // [HttpGet]
-        // public async Task<IEnumerable<Contato>> Get([FromRoute] string token) => await _context.contato.ToListAsync();
-        
-        // [HttpGet("{id}")]
-        // public async Task<IActionResult> GetContatoById([FromRoute] string token, [FromRoute] int id){
-        //     var valid_token = await _context.usuario.Where(user => user.Token == token).ToListAsync();
-
-        //     if(valid_token.Count == 0){
-        //         return NotFound();
-        //     }
-
-        //     var contato = await _context.contato.FindAsync(id);
-
-        //     return contato == null? NotFound(): Ok(contato);
-        // }
         [HttpGet("{id}")]
         public async Task<ActionResult<Contato>> GetContatoById([FromRoute] string token, [FromRoute] int id)
         {
@@ -50,27 +34,6 @@ namespace crud_back_end.Controllers{
             }
         }
 
-        // [HttpDelete("{id}")]
-        // [ProducesResponseType(StatusCodes.Status204NoContent)]
-        // [ProducesResponseType(StatusCodes.Status404NotFound)]
-        // public async Task<IActionResult> Delete([FromRoute] string token, int id){
-        //     var valid_token = await _context.usuario.Where(user => user.Token == token).ToListAsync();
-
-        //     if(valid_token.Count == 0){
-        //         return NotFound();
-        //     }
-
-        //     var deletarContato = await _context.contato.FindAsync(id);
-
-        //     if(deletarContato == null){
-        //         return NotFound();
-        //     }
-
-        //     _context.contato.Remove(deletarContato);
-        //     await _context.SaveChangesAsync();
-            
-        //     return NoContent();
-        // }
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteContato([FromRoute] string token, [FromRoute] int id)
         {
@@ -92,33 +55,6 @@ namespace crud_back_end.Controllers{
             }
         }
 
-        // [HttpPut("{id}")]
-
-        // public async Task<IActionResult> Update([FromRoute] string token, int id, UpdateContatoDTO updateContato){
-        //     var valid_token = await _context.usuario.Where(user => user.Token == token).ToListAsync();
-
-        //     if(valid_token.Count == 0){
-        //         return NotFound();
-        //     }
-
-        //     var contato = await _context.contato.FindAsync(id);
-
-        //     if (contato == null){
-        //         return NotFound();
-        //     }
-
-        //     contato.Nome = updateContato.Nome;
-        //     contato.Telefone = updateContato.Telefone;
-        //     contato.Email = updateContato.Email;
-        //     contato.Ativo = updateContato.Ativo;
-        //     contato.DataNascimento = updateContato.DataNascimento;
-        //     contato.DataEdicao = DateTime.UtcNow;
-
-        //     await _context.SaveChangesAsync();
-
-        //     return Ok(contato);
-        // }
-
         [HttpPut("{id}")]
         public async Task<ActionResult<Contato>> UpdateContato([FromRoute] string token, int id, UpdateContatoDTO updateContato)
         {
@@ -138,21 +74,6 @@ namespace crud_back_end.Controllers{
                 return NotFound(ex.Message);
             }
         }
-
-        // [HttpGet]
-        // [ProducesResponseType(typeof(Contato), StatusCodes.Status200OK)]
-        // [ProducesResponseType(StatusCodes.Status404NotFound)]
-        // public async Task<IActionResult> GetContato([FromRoute] string token){
-        //     var valid_token = await _context.usuario.Where(user => user.Token == token).FirstOrDefaultAsync();
-
-        //     if(valid_token == null){
-        //         return NotFound();
-        //     }
-
-        //     var contatos = await _context.contato.Where(cont => cont.UsuarioId == valid_token.Id).ToListAsync();
-
-        //     return Ok(contatos);
-        // }
 
         [HttpGet]
         public async Task<ActionResult<Contato>> GetContatos([FromRoute] string token)
@@ -174,30 +95,6 @@ namespace crud_back_end.Controllers{
             }
         }
 
-        // [HttpPost]
-        // [ProducesResponseType(StatusCodes.Status201Created)]
-        // public async Task<IActionResult> Create([FromRoute] string token, CreateContatoDTO createContato){
-        //     var valid_token = await _context.usuario.Where(user => user.Token == token).ToListAsync();
-
-        //     if(valid_token.Count == 0){
-        //         return NotFound();
-        //     }
-
-        //     var contato = new Contato();
-        //     contato.Nome = createContato.Nome;
-        //     contato.Telefone = createContato.Telefone;
-        //     contato.Email = createContato.Email;
-        //     contato.Ativo = createContato.Ativo;
-        //     contato.DataNascimento = createContato.DataNascimento;
-        //     contato.DataCadastro = DateTime.UtcNow;
-        //     contato.DataEdicao = null;
-        //     contato.UsuarioId = valid_token[0].Id;
-
-        //     await _context.contato.AddAsync(contato);
-        //     await _context.SaveChangesAsync();
-
-        //     return CreatedAtAction(nameof(GetContatoById), "Contato", new {token=token, id=contato.Id} , contato);
-        // }
         [HttpPost]
         public async Task<ActionResult<Contato>> CreateContato([FromRoute] string token, CreateContatoDTO createContato){
             try
